59a492f82c417462dde72af307a2c201dc8d7353
Gopir855 <144586454+Gopir855@users.noreply.github.com>
2024-09-24 12:52:34 +0530

    Update URL.java
    change in the UN

--- Zoho_Framework/src/test/java/zoho_base/URL.java
+++ Zoho_Framework/src/test/java/zoho_base/URL.java
@@ -4,6 +4,6 @@ public interface URL
 {
  String url="https://www.zoho.com/crm/lp/t-ee-logout.html";
  String lgURL="https://crm.zoho.in/crm/org60032809817/tab/Leads/custom-view/769975000000029342/list";
- String UN="kelvinjarden@gmail.com";
+ String UN="gopidealz@gmail.com";
  String PWD ="@Nokiaasha501";
 }
